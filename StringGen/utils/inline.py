1ab274c742926dd2ea994788530a2dfd0ae8b29e
Akash <158745842+Akash8t2@users.noreply.github.com>
2025-05-04 16:03:00 +0530

    Update inline.py

--- StringGen/utils/inline.py
+++ StringGen/utils/inline.py
@@ -9,7 +9,7 @@ keyboard = InlineKeyboardMarkup(
         [
             InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ", url=SUPPORT_CHAT),
             InlineKeyboardButton(
-                text="sᴏᴜʀᴄᴇ", url="https://t.me/Ak_support1"
+                text="sᴏᴜʀᴄᴇ", url="https://t.me/SFW_BotCore"
             ),
         ],
     ]
